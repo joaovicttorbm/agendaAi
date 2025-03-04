c68a8cc8687a411f74ea1dad9782ecc476a7a906
joaovicttorbm <joaovicttorbispo@gmail.com>
2025-03-03 21:28:12 -0300

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bjj-compass.vercel.app/api/" as string,
+  baseURL: import.meta.env.VITE_API_URL as string,
   headers: {
     "Content-Type": "application/json",
   },
